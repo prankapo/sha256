b3db7056700635eaf2ffff3c80183fe0f2f78e78
Praneet Kapoor <kapoorpraneet2619@gmail.com>
2021-12-24 22:20:31 +0530

    Added two comment lines to make sure that no one tinkers with the wrong lines

--- wrapper.py
+++ wrapper.py
@@ -4,10 +4,13 @@ if __name__ == "__main__":
 	#string = input(">>")
 	fp1 = open("test.txt", 'r')
 	string = fp1.read()
+	
+	#########DON'T CHANGE THIS!!#################
 	print("INPUT")
 	print(string)
 	string = './sha256.out "' + string + '"'
 	os.system(string)
+	#############################################
 	
 	fp2 = open("sha256_result.txt", 'r')
 	string = fp2.read()
